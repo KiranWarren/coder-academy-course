5bd1a4e743cbbde6dd09d7a2ba1e4641c2c3f153
Kiran Warren <kiran.m.warren@gmail.com>
2023-08-01 17:45:04 +1000

    Completed dynamic routes ed lesson

--- term-2/week-1/first_flask_app/app.py
+++ term-2/week-1/first_flask_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, abort
 import json
 from datetime import datetime
 from random import randint
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def homepage():
     return """<p>Hi, welcome to my API! Here are the endpoints that are available:</p>
         <ul>
-            <li>Current TimeL /time</li>
+            <li>Current Time: /time</li>
             <li>Educator Info: /educators</li>
         </ul>"""
 
@@ -43,3 +43,45 @@ def flip():
         "result": result
     }
     return json.dumps(coin_flip)
+
+@app.route("/some_value/<some_value>")
+def some_page(some_value):
+    return f"<p>You gave the value '{some_value}' in the route</p>"
+
+@app.route("/calculator/<int:int_1>/<string:operator>/<int:int_2>")
+def calculator(operator, int_1, int_2):
+    match operator:
+        case "add":
+            result = int_1 + int_2
+            operation = f"{int_1} plus {int_2}"
+            display_dict = {
+                "operation": operation,
+                "result": result
+            }
+            return json.dumps(display_dict)
+        case "subtract":
+            result = int_1 - int_2
+            operation = f"{int_1} minus {int_2}"
+            display_dict = {
+                "operation": operation,
+                "result": result
+            }
+            return json.dumps(display_dict)
+        case "divide":
+            result = int_1 / int_2
+            operation = f"{int_1} divided by {int_2}"
+            display_dict = {
+                "operation": operation,
+                "result": result
+            }
+            return json.dumps(display_dict)
+        case "multiply":
+            result = int_1 * int_2
+            operation = f"{int_1} multiplied by {int_2}"
+            display_dict = {
+                "operation": operation,
+                "result": result
+            }
+            return json.dumps(display_dict)
+        case _:
+            abort(404)
