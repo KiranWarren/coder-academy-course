d7bd732eb0bf8a185fd8c6f2ed89c7f984a2e120
Kiran Warren <kiran.m.warren@gmail.com>
2023-11-11 10:03:56 +1000

    get pic url function

--- term-3/image-download/downloader.js
+++ term-3/image-download/downloader.js
@@ -16,17 +16,37 @@ const { finished } = require("node:stream/promises");
 // /folder/subfolder/filename.png
 const path = require("node:path");
 
+// Constants
+API_URL_BASE = "https://pokeapi.co/api/v2/pokemon/";
+
 //
 function downloadPokePic(targetId = getRandomId()) {}
 
 // Generate a random number between 1 and 1017 (valid Pokemon Ids)
 function getRandomId() {
-  return 1 + Math.floor(Math.random * 1017);
+  return 1 + Math.floor(Math.random() * 1017);
 }
 
 // Retrieve Pokemon data for given id
 // Retrieve the image url from the Pokemon data
-async function getPokePicURL(targetId = getRandomId()) {}
+async function getPokePicURL(targetId = getRandomId()) {
+  // Retrieve the API
+  let response = await fetch(API_URL_BASE + targetId).catch((error) => {
+    throw new Error("API failure)");
+  });
+
+  if (response.status == "404") {
+    throw new Error("API did not have data for the requested ID");
+  }
+
+  // Convert the response into usable JSON
+  let data = await response.json().catch((error) => {
+    throw new Error("API did not return valid JSON");
+  });
+
+  // Return the image URL from the JSON data
+  return data.sprites.other["official-artwork"].front_default;
+}
 
 // Download that image and save it to the computer
 // Return the downloader image file path
