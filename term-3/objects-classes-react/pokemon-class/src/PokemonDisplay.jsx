cbd889ca47b4f7edfcf6d772ceab84e5eba83753
Kiran Warren <kiran.m.warren@gmail.com>
2023-11-18 11:13:14 +1000

    added PokemonDisplay component

---
+++ term-3/objects-classes-react/pokemon-class/src/PokemonDisplay.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default class PokemonDisplay extends React.Component {
+    constructor(props) {
+        super(props);
+
+        // state stores data per component
+        this.state = {
+            pokemonName: null
+        }
+    }
+
+    render() {
+        return (
+            <h2>Pokemon name</h2>
+        )
+    }
+}
