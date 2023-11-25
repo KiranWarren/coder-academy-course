fffcaccd18000316c4e02fc57e354b354a080fad
Kiran Warren <kiran.m.warren@gmail.com>
2023-11-25 10:17:27 +1000

    Added all routes

--- term-3/reactjs-forms/src/App.js
+++ term-3/reactjs-forms/src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import PokemonFetcherPage from "./pages/PokemonFetcherPage";
+import BusinessCardPage from "./pages/BusinessCardPage";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/pokemon" element={<PokemonFetcherPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/cards" element={<BusinessCardPage />} />
+      </Routes>
     </div>
   );
 }
