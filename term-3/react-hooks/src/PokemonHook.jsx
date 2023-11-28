d03a29723589914b291a53a210c2969ac8b6314a
Kiran Warren <kiran.m.warren@gmail.com>
2023-11-28 19:31:50 +1000

    fetching pokemon on open and button press

--- term-3/react-hooks/src/PokemonHook.jsx
+++ term-3/react-hooks/src/PokemonHook.jsx
@@ -1,7 +1,40 @@
+import { useEffect, useState } from "react";
+
 export default function PokemonHook() {
+  // = useState(initial value of state)
+  let [pokemon, setPokemon] = useState({});
+
+  function getPokemon() {
+    fetchData();
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  let fetchData = async () => {
+    let randomPokemonNumber = Math.floor(Math.random() * 1017) + 1;
+    let response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${randomPokemonNumber}`
+    );
+    let data = await response.json();
+    //setPokemon({name: data.name}) Can use if only 1 property
+    setPokemon(data);
+  };
+
+  if (pokemon.name) {
     return (
-        <div>
-            <h1>Feature</h1>
-        </div>
-    )
+      <div>
+        <h1>{pokemon.name}</h1>
+        <img src={pokemon.sprites.front_default} alt="Pokemon" />
+        <button onClick={getPokemon}>Get the Pokemon!</button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>No data to show.</h1>
+      </div>
+    );
+  }
 }
